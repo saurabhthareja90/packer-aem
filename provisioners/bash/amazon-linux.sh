fa3875ffc4bdca6fda8991238f903abbf2c5719b
Ramses Vejar <ramsesvejar@ramsess-mbp.lan>
2018-02-02 16:20:48 +1100

    installing puppet5, adding repo for python-cheetah, collectd and collectd-python

--- provisioners/bash/amazon-linux.sh
+++ provisioners/bash/amazon-linux.sh
@@ -3,12 +3,16 @@ set -o nounset
 set -o errexit
 
 yum -y upgrade
-rpm -ivh --force https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+
+# Installing puppet5, ony when its Amazon Linux 2 LTS Candidate AMI 2017.12.0 (HVM)
+sudo rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
 yum -y install puppet-agent
 
-# when installing puppet5, we got this error as Amazon Linux does not have systemd
-# Error: Package: puppet-agent-5.3.3-1.el7.x86_64 (puppet5) Requires: systemd
-#sudo rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
-#yum -y install puppet-agent
+# Required to install python-cheetah
+sudo rpm -ivh --force https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# none of the available repos have collectd or collectd-python. We had to add them manually :(
+sudo rpm -ivh --force https://buildlogs.centos.org/centos/7/opstools/x86_64/perfmon/collectd-5.8.0-1.el7.x86_64.rpm
+sudo rpm -ivh --force https://buildlogs.centos.org/centos/7/opstools/x86_64/perfmon/collectd-python-5.8.0-1.el7.x86_64.rpm
 
 exit 0
